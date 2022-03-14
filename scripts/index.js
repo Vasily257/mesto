26efa08960c71df34a21097099b65b2e2569dd8c
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-03-14 08:04:40 +0400

    refactor: remov the conversion to an array inside the input validation function

--- scripts/index.js
+++ scripts/index.js
@@ -67,7 +67,7 @@ function loadTextFromInput(display, input) {
 }
 
 function isEmptyInput(...arrayInput) {
-  return Array.from(arrayInput).some((input) => {
+  return arrayInput.some((input) => {
     return !input.value || input.value.trim() === '';
   });
 }
