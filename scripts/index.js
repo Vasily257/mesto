d07e5a7e9470f6e637280d2e23e93b2078fd8776
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-03-15 17:59:50 +0400

    feat: add a second pop-up and modifiers, create pop-up nodes

--- scripts/index.js
+++ scripts/index.js
@@ -10,12 +10,19 @@ const addButton = profile.querySelector('.profile__add-button');
 const cards = document.querySelector('.places');
 const cardsContainer = cards.querySelector('.places__list');
 
-const popup = document.querySelector('.popup');
-const formElement = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('.popup__input_type_name');
-const jobInput = popup.querySelector('.popup__input_type_activity');
-const saveButton = popup.querySelector('.popup__save-button');
-const closeButton = popup.querySelector('.popup__close-button');
+const editPopup = document.querySelector('.popup_type_edit');
+const formElementOfAddPopup = editPopup.querySelector('.popup__form_type_edit');
+const nameInput = editPopup.querySelector('.popup__input_type_name');
+const jobInput = editPopup.querySelector('.popup__input_type_activity');
+const saveButtonOfAddPopup = editPopup.querySelector('.popup__save-button_type_edit');
+const closeButtonOfAddPopup = editPopup.querySelector('.popup__close-button_type_edit');
+
+const addPopup = document.querySelector('.popup_type_add');
+const formElementOfEditPopup = addPopup.querySelector('.popup__form_type_add');
+const placeNameInput = addPopup.querySelector('.popup__input_type_place-name');
+const linkInput = addPopup.querySelector('.popup__input_type_link');
+const saveButtonOfEditPopup = addPopup.querySelector('.popup__save-button_type_add');
+const closeButtonOfEditPopup = addPopup.querySelector('.popup__close-button_type_add');
 
 const initialCards = [
   {
