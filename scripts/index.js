1b8dd9d5a89e3984bc5360d5724c09a87ec764df
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-04-14 18:54:18 +0400

    feat: move the `Card` class to Card.js and plug it to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 import { initialCards } from './cards.js';
+import Card from './Card.js';
 
 const profile = document.querySelector('.profile');
 const nameProfile = profile.querySelector('.profile__name');
@@ -91,70 +92,6 @@ popups.forEach((popup) => {
 });
 
 // Cards (places)
-
-class Card {
-  constructor(data) {
-    this._title = data.name;
-    this._link = data.link;
-    this._alt = data.name;
-  }
-
-  _getTemplate() {
-    const cardElement = document
-      .querySelector('.places-template')
-      .content.querySelector('.places__item')
-      .cloneNode(true);
-
-    return cardElement;
-  }
-
-  generateCard() {
-    this._element = this._getTemplate();
-    this._setEventListeners();
-
-    const cardTitle = this._element.querySelector('.places__title');
-    const cardImage = this._element.querySelector('.places__image');
-
-    cardTitle.textContent = this._title;
-    cardImage.src = this._link;
-    cardImage.alt = this._alt;
-
-    return this._element;
-  }
-
-  _setEventListeners() {
-    const buttonToLike = this._element.querySelector('.places__like-button');
-    const buttonToDelete = this._element.querySelector('.places__delete-button');
-    const buttonToEnlarge = this._element.querySelector('.places__enlarge-button');
-
-    buttonToLike.addEventListener('click', () => {
-      this._handleButtonToLike;
-    });
-
-    buttonToDelete.addEventListener('click', () => {
-      this._handleButtonToDelete;
-    });
-
-    buttonToEnlarge.addEventListener('click', () => {
-      this._handleButtonToEnlarge;
-    });
-  }
-
-  _handleButtonToLike() {
-    buttonToLike.classList.toggle('places__like-button_active');
-  }
-
-  _handleButtonToDelete() {
-    buttonToDelete.closest('.places__item').remove();
-  }
-
-  _handleButtonToEnlarge() {
-    popupImageCaption.textContent = this._title;
-    popupImage.src = this._link;
-    popupImage.alt = this._alt;
-    openPopup(popupEnlarging);
-  }
-}
 
 initialCards.forEach((card) => {
   const newCard = new Card(card);
