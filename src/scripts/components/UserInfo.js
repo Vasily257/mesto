2ddf0418a71bdf4fe4a4991e1bf0d3d134cad868
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-05-03 10:25:27 +0400

    feat: add the `UserInfo` class

---
+++ src/scripts/components/UserInfo.js
@@ -0,0 +1,20 @@
+export default class UserInfo {
+  constructor({ nameSelector, jobSelector }) {
+    this._name = document.querySelector(nameSelector);
+    this._job = document.querySelector(jobSelector);
+  }
+
+  getUserInfo() {
+    const userInfoProfile = {
+      name: this._name.textContent,
+      job: this._job.textContent,
+    };
+
+    return userInfoProfile;
+  }
+
+  setUserInfo({ name, job }) {
+    this._name.textContent = name.value;
+    this._job.textContent = job.value;
+  }
+}
