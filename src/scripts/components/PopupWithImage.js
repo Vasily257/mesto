fff8c9df8edf9a82a6216247bcf5b3b44c4ef826
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-05-04 09:31:07 +0400

    refactor: move elements of the enlarging popup to the class method and delete extra constants

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -1,17 +1,18 @@
 import Popup from './Popup.js';
-import { popupImage, popupImageCaption } from '../utils/constants.js';
 
 export default class PopupWithImage extends Popup {
   constructor(data, popupSelector) {
     super(popupSelector);
     this._name = data.name;
     this._link = data.link;
+    this._popupImage = this._popup.querySelector('.popup__image');
+    this._popupImageCaption = this._popup.querySelector('.popup__image-caption');
   }
 
   open() {
-    popupImageCaption.textContent = this._name;
-    popupImage.src = this._link;
-    popupImage.alt = this._name;
+    this._popupImageCaption.textContent = this._name;
+    this._popupImage.src = this._link;
+    this._popupImage.alt = this._name;
 
     super.open();
   }
--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -3,7 +3,3 @@ export const buttonForEditingProfile = profile.querySelector('.profile__edit-but
 export const buttonForAddingCard = profile.querySelector('.profile__add-button');
 
 export const cardsSelector = '.places__list';
-
-export const popupEnlarging = document.querySelector('.popup_type_enlarge');
-export const popupImage = popupEnlarging.querySelector('.popup__image');
-export const popupImageCaption = popupEnlarging.querySelector('.popup__image-caption');
