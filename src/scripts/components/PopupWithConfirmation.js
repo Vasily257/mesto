91960077564b3be9be8a9ec46da59dd22ea6dc22
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-05-28 13:44:48 +0400

    feat: add to close a submit popup when clicked Enter key

--- src/scripts/components/PopupWithConfirmation.js
+++ src/scripts/components/PopupWithConfirmation.js
@@ -5,10 +5,27 @@ export default class PopupWithConfirmation extends Popup {
     super(popupSelector);
     this._handleClickSubmit = {};
     this._submitButtonElement = this._popup.querySelector('.popup__submit-button');
+    this._handleEnterSubmit = this._handleEnterSubmit.bind(this);
   }
 
   setHandler(handler) {
     this._handleClickSubmit = handler;
+  }
+
+  _handleEnterSubmit(event) {
+    if (event.key === 'Enter') {
+      this._handleClickSubmit();
+    }
+  }
+
+  open() {
+    super.open();
+    document.addEventListener('keydown', this._handleEnterSubmit);
+  }
+
+  close() {
+    super.close();
+    document.removeEventListener('keydown', this._handleEnterSubmit);
   }
 
   setEventListeners() {
